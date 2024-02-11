bba47eee99e1c631db79ad2f3e95bb7f77cfd66c
Lamia Islam <lamiaislam108@gmail.com>
2024-02-11 15:52:10 +0600

    PHP

---
+++ lab4/task4_01.php
@@ -0,0 +1,13 @@
+<?php
+
+$length = 8; 
+$width = 9; 
+
+$area = $length * $width;
+
+$perimeter = 2 * ($length + $width);
+
+echo "The area of the rectangle is: " . $area . " square units\n";
+echo "The perimeter of the rectangle is: " . $perimeter . " units";
+
+?>

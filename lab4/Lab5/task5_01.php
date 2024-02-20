7a755a0cb72335e13e86f018963f29e582e95f5c
Lamia Islam <lamiaislam108@gmail.com>
2024-02-20 22:27:06 +0600

    form_validation

--- lab4/Lab5/task5_01.php
+++ lab4/Lab5/task5_01.php
@@ -1,16 +1,32 @@
 <?php
-    $username = $password ="";
+    $userName = $_REQUEST['username'];
+    $password = $_REQUEST['password'];
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $username = Validateusername($_POST["username"]);
-        $password = Validatepassword($_POST["password"]);
-    }
+    $isUserNameValid = true;
+    $isPasswordValid = true;
     
-    function Validateusername($username); {
-        $username = trim($username);
-        $len = strlen($username);
-        if($len < 2){
-            
-        }
-        return $data;
-     }
+    // validation for userName
+    if(strlen($userName) < 2){
+        $isUserNameValid = false;
+        echo 'Username must be atleast 2 characters <br>';
+    }
+    for($i = 0; $i < strlen($userName) ; $i++){
+        if( !(ord($userName[$i]) >= 65 && ord($userName[$i]) <= 90) && !(ord($userName[$i]) >= 97 && ord($userName[$i]) <= 122) && $userName[$i] != '.' && $userName[$i] != '-' && $userName[$i] != '_' ){
+            $isUserNameValid = false;
+            echo 'Invalid character in username <br>';
+            break; }
+    }
+     // validation for password
+    if(strlen($password) < 8){
+        $isPasswordValid = false;
+        echo 'Password must be atleast 8 characters <br>'; }
+    if(!str_contains($password, '@') && !str_contains($password, '#') && !str_contains($password, '$') && !str_contains($password, '%')){
+        $isPasswordValid = false;
+        echo 'Password must contain @, #, $ or % <br>'; }
+
+    if($isUserNameValid && $isPasswordValid) {
+        echo 'Valid user'; }
+    else{
+        echo 'Invalid user'; }
+
+?>

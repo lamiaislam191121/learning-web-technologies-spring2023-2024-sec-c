e9045dc426a61619d38cb19415a661be3caf341e
Lamia Islam <lamiaislam108@gmail.com>
2024-02-25 17:34:07 +0600

    lab6

---
+++ lab6/lab6_task4.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html land="en">
+<head>
+    <meta charset="UTF=8">
+    <meta name ="viewport" content="width=device-width, initial-scale=1">
+    <title>Forgot password</title>
+    <style>
+        table, th, td{
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
+</head>
+<body>
+<table border = "1" >
+        <th>
+            <h2 style="display: inline;">xCompany</h2>
+            <a href="#"> Home </a>
+            <a href="#"> Login </a>
+            <a href="#"> Registration </a>
+        </th>
+    <tr>
+        <td>
+        <fieldset>
+            <legend>Forgot password</legend>
+                 Email:<input type="email" name="email"> <hr> 
+            <input type="submit" name="submit">
+       </fieldset>
+    </td>
+    </tr>
+    <tr>
+        <td footer style="text-align: center;">copyright@2017</footer></td>
+    </tr>
+ </body>
+</head>
+</html>

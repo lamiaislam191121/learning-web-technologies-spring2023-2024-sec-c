55d0f20d3fd291b3bf4b38d85686d3ff73dd4f72
Lamia Islam <lamiaislam108@gmail.com>
2024-02-25 17:28:13 +0600

    lab6

---
+++ lab6/lab6_task1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Home | xCompany </title>
+    <style>
+        table, th, td{
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
+</head>
+<body>
+       <table border = "1" >
+        <th>
+            <h2 style="display: inline;">xCompany</h2>
+            <a href="#"> Home </a>
+            <a href="#"> Login </a>
+            <a href="#"> Registration </a>
+        </th>
+        <tr>
+            <td><h1>Welcome to xCompany</h1></td>
+        </tr>
+        <tr>
+            <td footer style="text-align: center;">copyright@2017</footer></td>
+        </tr>
+</body>
+</html>

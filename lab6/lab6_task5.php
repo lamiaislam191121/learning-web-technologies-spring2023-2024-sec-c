907c72e0c5d13504ff1173331046739981d4d832
Lamia Islam <lamiaislam108@gmail.com>
2024-02-25 17:34:21 +0600

    lab6

---
+++ lab6/lab6_task5.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Home | xCompany </title>
+    <style>
+        table, th, td{
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
+</head>
+<body>
+    <tr>
+       <table border = "1" >
+        <th>
+            <h2 style="display: inline;">xCompany</h2>
+            <a href="#"> Bob </a>
+            <a href="#"> Logout </a>
+        </th>
+        <tr>
+               <th colspan="1">
+                </th>  
+            </tr> 
+            <tr>
+                <td>
+                    <strong>Account</strong>
+                    <ul>
+                        <li><a href="./page-1.html">Dashboard</a></li>
+                        <li><a href="./page-1.html">View profile</a></li>
+                        <li><a href="./page-1.html">Edit profile</a></li>
+                        <li><a href="./page-1.html">Change profile picture</a></li>
+                        <li><a href="./page-1.html">Change password</a></li>
+                        <li><a href="./page-1.html">Logout</a></li>
+                    </ul>
+                </td>
+                <td>
+                    <h1>Welcome Bob</h1>
+                </td>
+            </tr>
+            <tr>
+            <td footer style="text-align: center;">copyright@2017</footer></td>
+            </tr>
+</body>
+</html>

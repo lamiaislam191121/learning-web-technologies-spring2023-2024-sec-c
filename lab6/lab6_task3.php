6b0d8472dbe15a848639212a5f8d7062885440e0
Lamia Islam <lamiaislam108@gmail.com>
2024-02-25 17:33:40 +0600

    lab6

---
+++ lab6/lab6_task3.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html land="en">
+<head>
+    <meta charset="UTF=8">
+    <meta name ="viewport" content="width=device-width, initial-scale=1">
+    <title>LOGIN</title>
+    <style>
+        table, th, td{
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
+</head>
+<body>
+<table border = "1" >
+        <th>
+            <h2 style="display: inline;">xCompany</h2>
+            <a href="#"> Home </a>
+            <a href="#"> Login </a>
+            <a href="#"> Registration </a>
+        </th>
+    <tr>
+        <td>
+        <fieldset>
+            <legend>Login</legend>
+                 user Name: <input type="text" name="username"><br>
+                 Password: <input type="password" name="password"><hr>
+            <input type="checkbox" name="remember"> Remember me <br> 
+            <input type="submit" name="submit">
+            <a href="#">Forget Password?</a>
+       </fieldset>
+    </td>
+    </tr>
+    <tr>
+        <td footer style="text-align: center;">copyright@2017</footer></td>
+    </tr>
+ </body>
+</head>
+</html>

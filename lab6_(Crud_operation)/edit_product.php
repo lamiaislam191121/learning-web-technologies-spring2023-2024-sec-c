fc9404c38c95fd9b9e1b509bd1b3ad5fab7bc4ce
Lamia Islam <lamiaislam108@gmail.com>
2024-03-07 16:34:35 +0600

    lab_(crud_operation)

---
+++ lab6_(Crud_operation)/edit_product.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html land="en">
+<head>
+    <meta charset="UTF=8">
+    <meta name ="viewport" content="width=device-width, initial-scale=1">
+    <title>Edit Product</title>
+    <style>
+        table, th, td{
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+    </style>
+</head>
+<body>
+<table border = "1" >
+    <tr>
+        <td>
+            <fieldset>
+                <legend>Add Product</legend>
+                    Name <br> <input type="text" name="name"></br>
+                    Buying price <br> <input type="number" name="Buying price"></br>
+                    Selling price <br> <input type="number" name="Selling price"></br>
+                    <hr>
+                    <input type="checkbox" name="Display"> Display <br> 
+                    <input type="button" name="save" VALUE ="SAVE">
+             </fieldset>
+         </td>
+    </tr>
+</body>
+</head>
+</html>
